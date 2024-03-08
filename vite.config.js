aa8331fcba9717aa7054802fd4c783430697b167
nanda99kumar <34065087+nanda99kumar@users.noreply.github.com>
2024-03-08 00:24:41 -0500

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
- base: '/nanda99kumar.github.io/',
-  plugins: [react()],
-
-})
